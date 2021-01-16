d0837dba035682002b739c98e7b6e54b6084dea7
Artem_Svistelnik <artem.svistelnik@gmail.com>
2021-01-16 16:00:48 +0200

    first commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,3 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('coock_blog/', include('blog.urls', namespace='blog'))
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
